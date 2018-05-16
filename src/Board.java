044e0c34546c5c79e2fda7728f3bccc9230d6775
3jackattack3 <3jackattack3@gmail.com>
2018-05-15 21:56:51 -0400

    created ship and board classes

---
+++ src/Board.java
@@ -0,0 +1,11 @@
+/*
+    Jack Defay
+    Board.java
+    5/15/2018
+*/
+
+public class Board {
+    public static void main(String args[]) {
+
+    }
+}
---
+++ src/Ship.java
@@ -0,0 +1,11 @@
+/*
+    Jack Defay
+    Ship.java
+    5/15/2018
+*/
+
+public class Ship {
+    public static void main(String args[]) {
+
+    }
+}
