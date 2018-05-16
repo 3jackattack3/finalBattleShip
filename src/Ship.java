d3bd6784585ab7307429211607becf89c9bac1ff
3jackattack3 <3jackattack3@gmail.com>
2018-05-15 22:54:35 -0400

    added coordinate thing

--- src/Ship.java
+++ src/Ship.java
@@ -9,13 +9,19 @@ import java.util.ArrayList;
 public class Ship {
     private ArrayList<Integer[]> coordinates;
 
+    //private lives
+
     public Ship(int[] startingCoords, int length, int[] direction){
         coordinates = new ArrayList<Integer[]>();
-        int[] temp = new int[2];
+        Integer[] temp = new Integer[2];
 
         for(int i = 0; i < length; i++){
-
-            coordinates.add();
+            temp[0] = new Integer(startingCoords[0] + i * direction[0]);
+            temp[1] = new Integer(startingCoords[1] + i * direction[1]);
+            coordinates.add(temp);
         }
     }
+
+    //check hit
+    //check destroyed
 }
