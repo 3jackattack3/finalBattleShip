86460c8b750ee25420a3e00bcbc96adbaf8644c2
3jackattack3 <3jackattack3@gmail.com>
2018-05-15 23:33:46 -0400

    realized the coordinate arrayList should actually be an array

--- src/Ship.java
+++ src/Ship.java
@@ -7,22 +7,22 @@
 import java.util.ArrayList;
 
 public class Ship {
-    private ArrayList<Integer[]> coordinates;
+    private int[][] coordinates;
 
     //private lives
 
     public Ship(int[] startingCoords, int length, int[] direction){
-        coordinates = new ArrayList<Integer[]>();
-        Integer[] temp = new Integer[2];
+        coordinates = new int[length][2];
+        int[] temp = new int[2];
 
         for(int i = 0; i < length; i++){
-            temp[0] = new Integer(startingCoords[0] + i * direction[0]);
-            temp[1] = new Integer(startingCoords[1] + i * direction[1]);
-            coordinates.add(temp);
+            temp[0] = startingCoords[0] + i * direction[0];
+            temp[1] = startingCoords[1] + i * direction[1];
+            coordinates[i] = temp;
         }
     }
 
-    public ArrayList<Integer[]> getCoordinates(){
+    public int[][] getCoordinates(){
         return coordinates;
     }
 
